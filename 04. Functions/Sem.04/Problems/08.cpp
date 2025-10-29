729905ab35adc0c121bb06b4c6d40baf5ac7891a
bojidarapartenova <bpartenova@gmail.com>
2025-10-29 22:43:11 +0200

    Add All Problems Solutions

---
+++ 04. Functions/Sem.04/Problems/08.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+bool equalDigits(int n)
+{
+    int prevDigit = n % 10;
+    n /= 10;
+
+    while (n != 0)
+    {
+        int currentDigit = n % 10;
+
+        if (currentDigit != prevDigit)
+        {
+            return false;
+        }
+        prevDigit = currentDigit;
+        n /= 10;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::cout << equalDigits(n);
+}
