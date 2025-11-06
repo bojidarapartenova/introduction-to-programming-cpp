bf965884c59de42f3645d2ac242a259b222afab4
bojidarapartenova <bpartenova@gmail.com>
2025-11-06 13:04:28 +0200

    Add One-dimensional arrays Solutions

--- Arrays/Sem.05/01.cpp
+++ Arrays/Sem.05/01.cpp
@@ -46,19 +46,95 @@ void sortArray(int arr[SIZE_OF_ARRAY])
     }
 }
 
-// int binarySearch(int arr[SIZE_OF_ARRAY], int n)
-// {
-//     sortArray(arr);
-//     int mid = arr[SIZE_OF_ARRAY / 2];
-// }
+void binarySearch(int arr[SIZE_OF_ARRAY], int n)
+{
+    sortArray(arr);
+    std::cout << "Sorted array: ";
+    printArray(arr);
+    std::cout << std::endl;
+
+    int start = 0;
+    int end = SIZE_OF_ARRAY - 1;
+
+    while (start <= end)
+    {
+        int mid = start + end / 2;
+
+        if (n == arr[mid])
+        {
+            std::cout << "The element is at index: " << mid;
+            break;
+        }
+
+        if (n > arr[mid])
+        {
+            start = mid + 1;
+        }
+
+        if (n < arr[mid])
+        {
+            end = mid - 1;
+        }
+    }
+}
+
+void bubbleSort(int arr[SIZE_OF_ARRAY])
+{
+    for (int i = 0; i < SIZE_OF_ARRAY - 1; i++)
+    {
+        for (int j = 0; j < SIZE_OF_ARRAY - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+
+    printArray(arr);
+}
+
+void sieveOfEratosthenes(int n)
+{
+    bool isPrime[n + 1];
+
+    isPrime[1] = false;
+
+    for (int i = 2; i <= n; i++)
+    {
+        isPrime[i] = true;
+    }
+
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (isPrime[i])
+        {
+            std::cout << i << " ";
+        }
+    }
+}
 
 int main()
 {
-    int arr[SIZE_OF_ARRAY] = {5, 4, 3, 2, 1};
+    int arr[SIZE_OF_ARRAY] = {3, 4, 1, 6, 2};
 
     // printArray(arr);
     // linearSearch(arr, 3);
     // reverseArray(arr);
-    sortArray(arr);
-    printArray(arr);
+    // binarySearch(arr, 3);
+    // bubbleSort(arr);
+    // sieveOfEratosthenes(10);
 }
