5899e47e53d2e73e80986caaddc97ecb00052857
bojidarapartenova <bpartenova@gmail.com>
2025-11-10 23:42:24 +0200

    Add printIntersection

---
+++ Arrays/Pract.05/02.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+void printArray(int size, int arr[])
+{
+    std::cout << "[";
+    for (int i = 0; i < size; i++)
+    {
+        if (i < size - 1)
+        {
+            std::cout << arr[i] << " ";
+        }
+        else
+        {
+            std::cout << arr[i] << "]";
+        }
+    }
+}
+
+void printIntersection(int n, int arr1[], int m, int arr2[], int result[])
+{
+    int resultPos = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        bool isAdded = false;
+        for (int j = 0; j < resultPos; j++)
+        {
+            if (result[j] == arr1[i])
+            {
+                isAdded = true;
+                break;
+            }
+        }
+
+        if (isAdded)
+        {
+            continue;
+        }
+
+        for (int k = 0; k < m; k++)
+        {
+            if (arr1[i] == arr2[k])
+            {
+                result[resultPos++] = arr1[i];
+                break;
+            }
+        }
+    }
+    printArray(resultPos, result);
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    int arr1[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr1[i];
+    }
+
+    int m;
+    std::cin >> m;
+
+    int arr2[m];
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> arr2[i];
+    }
+
+    int result[100];
+
+    printIntersection(n, arr1, m, arr2, result);
+}
